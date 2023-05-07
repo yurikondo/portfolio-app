f0924ae0500a3e0d1612e83ae14304fe1f5a6066
yuri <yuri@example.com>
2023-05-07 09:33:11 +0900

    Postのルーティング

--- server/src/v1/routes/index.js
+++ server/src/v1/routes/index.js
@@ -4,5 +4,6 @@ const router = require("express").Router();
 //レッスン52より
 router.use("/auth", require("./auth"));
 router.use("/memo", require("./memo"));
+router.use("/post", require("./post"));
 
 module.exports = router;
