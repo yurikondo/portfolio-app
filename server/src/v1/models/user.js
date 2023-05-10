51998f5fb7a9915a6cc0c1bc3b7b652d6f01294d
yuri <yuri@example.com>
2023-05-10 12:54:39 +0900

    userモデルにbgImgカラムを追加

--- server/src/v1/models/user.js
+++ server/src/v1/models/user.js
@@ -16,6 +16,12 @@ const userSchema = new mongoose.Schema({
     default: "🙂",
     required: true,
   },
+  bgImg: {
+    type: String,
+    default: "https://pixabay.com/get/g2a9f517799f1959586ae1429b2c076511317c68d484e8fd293c1deb38df644436eaac5e23ca9f4b68ca70ab6d8aca97c54ad6542382a7cb6dcb4c23497db360a_1280.jpg",
+    required: true,
+  },
+
 });
 
 module.exports = mongoose.model("User", userSchema);
