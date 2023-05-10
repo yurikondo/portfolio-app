f707a1142a0c7dc38f936e14928289cf4c15f0c6
yuri <yuri@example.com>
2023-05-10 12:55:10 +0900

    不要なコードの削除

--- client/src/components/common/ImageGallery.jsx
+++ client/src/components/common/ImageGallery.jsx
@@ -5,12 +5,12 @@ const ImageGallery = (props) => {
   const { fetchData } = props;
 
   return (
-    <ImageList sx={{ width: "100%", height: "100%" }} cols={3} rowHeight={16}>
+    <ImageList sx={{ width: "100%", height: "100%" }} cols={3} rowHeight={164}>
       {fetchData.map((data) => (
-        <ImageListItem key={data.id}>
+        <ImageListItem key={data.id} sx={{ cursor: "pointer" }}>
           <img
-            src={`${data.webformatURL}?w=164&h=164&fit=crop&auto=format`}
-            srcSet={`${data.webformatURL}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            src={`${data.webformatURL}?w=164&h=100&fit=crop&auto=format`}
+            srcSet={`${data.webformatURL}?w=164&h=100&fit=crop&auto=format&dpr=2 2x`}
             alt="画像"
             loading="lazy"
           />
