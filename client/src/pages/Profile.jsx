e8f0fdba268e19693ef1b3d5f9045278273d27f4
yuri <yuri@example.com>
2023-05-10 12:55:27 +0900

    デフォルト背景画像の変更

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,7 +13,7 @@ const Profile = () => {
     userIcon: "😊",
     accountCreated: "2023/05/01",
     backgroundImage:
-      "url(https://pixabay.com/get/gc7d91fd704e08d39c6dfb3d7b5b71d31d2713b9f5d3413bdfcddb150441f7286e049673ea7fc6a2bb12872356f7b0d1265d86f7be07c5dca228f9010a7aa9756_1280.jpg)",
+      "url(https://pixabay.com/get/g2a9f517799f1959586ae1429b2c076511317c68d484e8fd293c1deb38df644436eaac5e23ca9f4b68ca70ab6d8aca97c54ad6542382a7cb6dcb4c23497db360a_1280.jpg)",
     postCount: 10,
   };
 
