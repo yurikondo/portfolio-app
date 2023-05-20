39cbd8f2ab2c0b1b3f47a0a141b832634a258693
yuri <yuri@example.com>
2023-05-20 19:06:40 +0900

    DBのSchemaに項目を追加

--- server/src/v1/models/post.js
+++ server/src/v1/models/post.js
@@ -1,25 +1,36 @@
 //モデルの作成（https://mongoosejs.com/docs/models.html#compiling）
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-  //user情報と連携（typeとrefはセットで記述）
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const postSchema = new mongoose.Schema(
+  {
+    //user情報と連携（typeとrefはセットで記述）
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    itemURL: {
+      type: String,
+      required: true,
+    },
+    itemImgURL: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
   },
-  itemURL: {
-    type: String,
-    required: true,
-  },
-  title: {
-    type: String,
-    default: "",
-  },
-  desc: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Post", postSchema);
--- server/src/v1/models/user.js
+++ server/src/v1/models/user.js
@@ -1,27 +1,46 @@
 //モデルの作成（https://mongoosejs.com/docs/models.html#compiling）
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      default: "🙂",
+      required: true,
+    },
+    bgImg: {
+      type: String,
+      default:
+        "https://pixabay.com/get/g8e041aa4548c8a7c93a04000433929f7d8a03ecb6cbd0e7458921d2f003a990c392343a0b4695449a47589dcbc569e6cdb82812ae03ee798fcc1b50264f3a3b2_640.jpg?w=164&h=100&fit=crop&auto=format&dpr=2",
+      required: true,
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    desc: {
+      type: String,
+      max: 20,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  icon: {
-    type: String,
-    default: "🙂",
-    required: true,
-  },
-  bgImg: {
-    type: String,
-    default: "https://pixabay.com/get/g8e041aa4548c8a7c93a04000433929f7d8a03ecb6cbd0e7458921d2f003a990c392343a0b4695449a47589dcbc569e6cdb82812ae03ee798fcc1b50264f3a3b2_640.jpg?w=164&h=100&fit=crop&auto=format&dpr=2",
-    required: true,
-  },
-
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
