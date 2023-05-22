3efdac7cefbe586a460cdd8cc17bc0983b276ed3
yuri <yuri@example.com>
2023-05-22 20:31:41 +0900

    プロフィール画面にカードとユーザーコンポーネントを追加

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 import userApi from "../api/userApi";
 import EmojiPicker from "../components/common/pickers/EmojiPicker";
 import BgImgPicker from "../components/common/pickers/BgImgPicker";
-import { Box, Button, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import MainCard from "../components/common/MainCard";
+import postApi from "../api/postApi";
+import UserListItem from "../components/common/UserListItem";
+import { setPost } from "../redux/features/postSlice";
 import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
-import defaultBgImg from "../assets/images/defaultBgImg.jpg";
 import { grey } from "@mui/material/colors";
+import { Box, Button, Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 // import InputDesc from "../components/common/InputDesc";
 
 const Profile = () => {
@@ -14,6 +18,23 @@ const Profile = () => {
   const [bgImg, setBgImg] = useState("");
   const [isShowBgImgPicker, setIsShowBgImgPicker] = useState(false);
   const user = useSelector((state) => state.user.value);
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.post.value);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const res = await postApi.getAll();
+        dispatch(setPost(res));
+        // setPosts(res);
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPosts();
+  }, [dispatch]);
+
 
   useEffect(() => {
     if (user.bgImg) {
@@ -91,6 +112,23 @@ const Profile = () => {
           />
         </Box>
       </Box>
+      <Grid container spacing={3} sx={{mt: 2}}>
+        <Grid item xs={8}>
+          {posts.map((post) => (
+            <MainCard
+              key={post._id}
+              postId={post._id}
+              desc={post.desc}
+              itemImgURL={post.itemImgURL}
+              userId={post.user}
+              createdAt={post.createdAt}
+            />
+          ))}
+        </Grid>
+        <Grid item xs={4}>
+          <UserListItem />
+        </Grid>
+      </Grid>
     </Box>
   );
 };
