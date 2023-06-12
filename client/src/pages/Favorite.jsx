5755c61244b7544ce79bc92c9f9c903c406c3160
yuri <yuri@example.com>
2023-06-12 18:45:57 +0900

    まだ投稿にいいねした投稿がない時のメッセージを表示

--- client/src/pages/Favorite.jsx
+++ client/src/pages/Favorite.jsx
@@ -4,9 +4,10 @@ import { useSelector } from "react-redux";
 import MainCard from "../components/common/maincard/MainCard";
 import postApi from "../api/postApi";
 import { Box, Grid } from "@mui/material";
+import ErrorText from "../components/common/ErrorText";
 
 const Favorite = () => {
-  const [posts, setPosts] = useState([]);
+  const [likedPosts, setLikedPosts] = useState([]);
   const loginUser = useSelector((state) => state.user.value);
   const navigate = useNavigate();
 
@@ -18,8 +19,7 @@ const Favorite = () => {
     const getPosts = async () => {
       try {
         const res = await postApi.getLikedPosts();
-        setPosts(res);
-        console.log(res);
+        setLikedPosts(res);
       } catch (err) {
         console.log(err);
       }
@@ -30,7 +30,12 @@ const Favorite = () => {
   return (
     <Box>
       <Grid container spacing={3} sx={{ mt: 3 }}>
-        {posts.map((post) => (
+        {likedPosts.length === 0 && (
+          <Grid item xs={12}>
+            <ErrorText text="まだいいねした投稿はありません" />
+          </Grid>
+        )}
+        {likedPosts.map((post) => (
           <Grid item xs={6}>
             <MainCard key={post._id} post={post} />
           </Grid>
